97b94fd3b5b25e8a2b957388ab1fa3fc85e09d87
StevePy <sokaku@zoho.com>
2017-07-06 10:15:29 +1000

    Reverted incorrect .ToString for test using object reference. Corrected date assignment to avoid issue where -0 days + x seconds could return false-positives.

--- Src/System.Linq.Dynamic.Tests/EntitiesTests.cs
+++ Src/System.Linq.Dynamic.Tests/EntitiesTests.cs
@@ -69,7 +69,7 @@ namespace System.Linq.Dynamic.Tests
                         Blog = blog,
                         Title = String.Format("Blog {0} - Post {1}", i + 1, j + 1),
                         Content = "My Content",
-                        PostDate = DateTime.Today.AddDays(-Rnd.Next(0,100)).AddSeconds(Rnd.Next(0, 30000)),
+                        PostDate = DateTime.Today.AddDays(-Rnd.Next(1,100)).AddSeconds(Rnd.Next(0, 30000)),
                         PostGlobalId = Guid.NewGuid(),
                         NumberOfReads = Rnd.Next(0, 5000)
                     };
@@ -173,7 +173,7 @@ namespace System.Linq.Dynamic.Tests
             var expected = _context.Posts.Where(x => x.PostDate > utcNow).ToArray();
 
             //Act
-            var test = _context.Posts.Where("PostDate > @0", utcNow.ToString("u")).ToDynamicArray();
+            var test = _context.Posts.Where("PostDate > @0", utcNow).ToDynamicArray();
             
             //Assert
             CollectionAssert.AreEqual(expected, test);
@@ -192,7 +192,7 @@ namespace System.Linq.Dynamic.Tests
             var test = _context.Posts.Where("PostDate > DateTime(\"" + utcNow.ToString("u") + "\")").ToDynamicArray();
 
             //Assert
-            CollectionAssert.AreEqual(expected, test);
+            CollectionAssert.AreEqual(expected, test, string.Format("expected: {0} found: {1}", expected.Length, test.Length));
         }
 
         [TestMethod]
@@ -207,11 +207,11 @@ namespace System.Linq.Dynamic.Tests
             //Act
             var test = _context.Posts.Where("PostDate > DateTime\"" + utcNow.ToString("u") + "\"").ToDynamicArray();
 
-            //Assert
-            CollectionAssert.AreEqual(expected, test);
-        }
+			//Assert
+			CollectionAssert.AreEqual(expected, test, string.Format("expected: {0} found: {1}", expected.Length, test.Length));
+		}
 
-        [TestMethod]
+		[TestMethod]
         public void Entities_Where_ByGuid_AsObject()
         {
             //Arrange
